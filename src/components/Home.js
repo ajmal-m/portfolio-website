2cfa2c2e626ab8354cf5042054dad77ed917e79d
ajmal5410347 <ajmal@airtory.com>
2025-01-11 22:04:51 +0530

    [P-1] Resume download added

--- src/components/Home.js
+++ src/components/Home.js
@@ -42,10 +42,20 @@ const Home = ()  => {
                 <div>
                     <button type="button" 
                         className="
+                            transition-all duration-300 ease-in-out
                             min-w-[172px] h-[40px] flex items-center 
                             justify-center border px-[16px]
-                            gap-[0.5rem]
+                            gap-[0.5rem] rounded-[12px] border-dashed
+                            hover:border-solid hover:bg-nameColor
                         "
+                        onClick={() => {
+                            const link = document.createElement('a');
+                            link.href = './file/dsa.pdf'; 
+                            link.download = 'resume.pdf';
+                            document.body.appendChild(link);
+                            link.click();
+                            document.body.removeChild(link);
+                          }}
                     > 
                         <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#d1d5db">
                             <path d="M480-320 280-520l56-58 104 104v-326h80v326l104-104 56 58-200 200ZM240-160q-33 0-56.5-23.5T160-240v-120h80v120h480v-120h80v120q0 33-23.5 56.5T720-160H240Z"/>
