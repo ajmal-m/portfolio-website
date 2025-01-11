8331c3f7c945689d6269e732fa54e0dc968658de
ajmal5410347 <ajmal@airtory.com>
2025-01-11 11:55:49 +0530

    [P-1] dark and light theme swithing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import logo from './logo.svg';
+import ThemeSwitcher from './components/ThemeSwitcher';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p class="text-[red] text-[40px]">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App h-screen flex items-center justify-center bg-[grey] dark:bg-[red]">
+      <ThemeSwitcher/>
     </div>
   );
 }
