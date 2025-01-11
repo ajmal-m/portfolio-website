1158a125f12e629513bd53a8f13e5b930de169db
ajmal5410347 <ajmal@airtory.com>
2025-01-11 14:44:47 +0530

    [p-1] font fam updates

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -25,15 +25,30 @@ const Navbar = () => {
     return (
         <nav 
             className="
-                mt-[24px] px-[16px] max-w-[600px] min-w-[300px] 
-                max-h-[34px] rounded-[20px] bg-[black] items-center 
+                mt-[24px] px-[16px] max-w-[600px] min-w-[600px] 
+                max-h-[56px] rounded-[1rem] bg-[black] items-center 
                 flex justify-between backdrop-blur-lg bg-opacity-40
             "
         >
             {
                 navItems && navItems.map((item, index) => (
-                    <div  key={index} className="flex">
-                        <a href={`/${item?.id}`} className="text-[white] font-[500] text-[12px]">{item?.name}</a>
+                    <div  
+                        key={index} 
+                        className="
+                            flex p-[4px] rounded-[0.5rem]
+                        "
+                    >
+                        <a 
+                            href={`/${item?.id}`} 
+                            className="
+                                transition-all duration-300 ease-in-out
+                                text-[white] font-[600] text-[12px] font-popinsMedium
+                                px-[12px] py-[6px] bg-blue-btn 
+                                rounded-[0.5rem] bg-opacity-50
+                            "
+                        >
+                            {item?.name}
+                        </a>
                     </div>
                 ))
             }
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,12 +8,22 @@ module.exports = {
       // background colors
       'backgroundImage':{
         'blue-shadow':'radial-gradient(circle at 1003px 96px, rgb(10, 31, 68), rgb(9, 28, 61), rgb(8, 24, 53), rgb(7, 21, 45), rgb(5, 18, 37), rgb(4, 15, 30), rgb(3, 12, 23), rgb(2, 9, 17))',
+        'blue-btn':'linear-gradient(86deg, #60a5fa, transparent)'
       },
 
 
       // colors
       'colors':{
-        'primary':'#fff'
+        'primary':'#fff',
+        'primaryBlue':'#60a5fa'
+      },
+
+
+      // Font family
+      'fontFamily':{
+        'popinsMedium':["PoppinsMedium"],
+        'brandaMedium':["Branda"],
+        'jumbMedium':['HigherJump']
       }
     },
   },
